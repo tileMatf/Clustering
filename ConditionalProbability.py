2b962e651256e7a62b22a4ab3fc7353328fedf09
TijanaJakoveljvic <tijana.jakovljevic12@gmail.com>
2016-05-25 00:01:48 +0200

    little changes, like changed round from 3 to 2...

--- ConditionalProbability.py
+++ ConditionalProbability.py
@@ -16,35 +16,35 @@ def ConditionalProbability(Parameters, Data, allShows):
     else :
       HiddenVector[k]=0
  
-  print(HiddenVector[0:len(HiddenVector)])
+  #print(HiddenVector)
   return HiddenVector
-  
-  
+
   
 #matrixHiddenVector
 def EStep(Parameters, Data, allShows):
   w, h =len(Data), 2
   matrix = [[0 for x in range(w)] for y in range(h)] 
   
-  for i in matrix:
-    print i  
+  #for i in matrix:
+   # print i  
  
   for k in range (0, len(Data)):
     pom1=pow(Parameters[0],allShows*Data[k])*pow(1-Parameters[0],allShows*(1-Data[k]))
     pom2=pow(Parameters[1],allShows*Data[k])*pow(1-Parameters[1],allShows*(1-Data[k]))
-    a=round(pom1/(pom1+pom2), 3)
-    b=round(pom2/(pom1+pom2), 3)
+    a=round(pom1/(pom1+pom2), 2)
+    b=round(pom2/(pom1+pom2), 2)
     matrix[0][k]=a
     matrix[1][k]=b
     
-  for i in matrix:
-    print i
+  #for i in matrix:
+  #  print i
   
   return matrix  
+
   
 #example
-Parameters=[0.6, 0.83]
-Data=[0.4, 0.9, 0.8, 0.3, 0.7]
-allShows=10
-ConditionalProbability(Parameters, Data, allShows)
-EStep(Parameters, Data, allShows)
+#Parameters=[0.6, 0.83]
+#Data=[0.4, 0.9, 0.8, 0.3, 0.7]
+#allShows=10
+#ConditionalProbability(Parameters, Data, allShows)
+#EStep(Parameters, Data, allShows)
