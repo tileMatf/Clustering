2830547c25fcdfb1746e3c4f81293840311da477
TijanaJakoveljvic <tijana.jakovljevic12@gmail.com>
2016-05-27 13:26:39 +0200

    added minDistance function

--- NearestCenter.py
+++ NearestCenter.py
@@ -14,3 +14,17 @@ def NearestCenter(Point, Centers):
 	return [Centers[minDistanceIndex], minDistance, minDistanceIndex]
 
 
+#return min distance of two points in different clusters
+def MinDistance(Cluster1, Cluster2):
+  minDistance=Euclidean.EuclideanDistance(Cluster1[0], Cluster2[0])
+  #print (minDistance)
+  
+  for i in range (0, len(Cluster1)):
+    for j in range (0, len(Cluster2)):
+      currentDistance=Euclidean.EuclideanDistance(Cluster1[i], Cluster2[j])
+      #print(i, j, currentDistance)
+      if(currentDistance < minDistance):
+        minDistance=currentDistance
+        
+  #print(minDistance, Cluster1[i], i, Cluster2[j], j)
+  return minDistance      
